8d6da27a94de34ee2d05779dd8b580d7ddefae59
esteregg <kamil.gadawski@gmail.com>
2020-02-05 21:53:36 +0100

    Added CSS Style

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="wrapper>">
     <div class="search">
-      <label>Search</label>
-      <input v-model="searchCity" type="text" placeholder="City" @input="toogleInput">
-      <div class="weather-results">
+      <input v-model="searchCity" class="search-input" type="text" placeholder="City" @input="toogleInput">
+      <div class="weather-results" v-if="loading">
         <h2>City: {{ city }} {{ country }}</h2>
         <p>Temperature: {{ Math.round(temp) }} *C</p>
         <p>Temperature min.: {{ Math.round(tempMin) }} *C</p>
@@ -30,6 +29,7 @@ export default {
       tempMax: '',
       pressure: '',
       country: '',
+      loading: false,
       TOKEN: '8d410a8f575db366b3ee6cf4861281ac',
       URL_API: 'https://api.openweathermap.org/data/2.5/weather'
 
@@ -39,7 +39,6 @@ export default {
     toogleInput: lodash.debounce(function() {
       axios.get(`${this.URL_API}?q=${this.searchCity}&units=metric&appid=${this.TOKEN}`)
         .then((response) => {
-          console.log(response)
           this.city = response.data.name
           this.temp = response.data.main.temp
           this.tempMax = response.data.main.temp_max
@@ -47,8 +46,11 @@ export default {
           this.feelsLike = response.data.main.feels_like
           this.pressure = response.data.main.pressure
           this.country = response.data.sys.country
+
+          this.loading = true
         })
         .catch((e) => {
+          this.loading = false
           console.error(e)
         })
     }, 500)
@@ -57,11 +59,30 @@ export default {
 
 </script>
 <style scoped>
+p {
+  font-size: large;
+}
 .wrapper,
 .search {
+  margin-top: 10vh;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.search-input {
+  width: 200px;
+  height: 30px;
+  border: solid;
+  border-radius: 10px 0 10px 0;
+  border-width: 2px;
+  padding-left: 8px;
+  font-size: medium;
+}
+
+.search-input:focus {
+  transition-duration: 500ms;
+  border-radius: 0 10px 0 10px;
 }
 
 </style>
